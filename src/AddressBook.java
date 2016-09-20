0c3726cf72a15c8072e3bb3243a9fd73425ec1d5
damanjitbhangoo <damanjitbhangoo@aa0508-50.labs.sce.carleton.ca>
2016-09-20 12:17:25 -0400

    added more functions

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,19 +2,26 @@ import java.util.*;
 
 public class AddressBook {
 
-	ArrayList<BuddyInfo> list = new ArrayList<BuddyInfo>();
-	
-	public void removeBuddy(BuddyInfo x) {
-		
-	}
+	static ArrayList<BuddyInfo> list = new ArrayList<BuddyInfo>();
 	
 	public void addBuddy(BuddyInfo x) {
-		
+		if (x != null ){
+			list.add(x);
+		}
+	}
+	
+	public void removeBuddy(int x) {
+		if (x >= 0 && x <list.size() ){
+			list.remove(x);
+		}
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BuddyInfo x = new BuddyInfo("hi");
+		AddressBook AB = new AddressBook();
 		System.out.println("Hello world!");
+		AB.addBuddy(x);
 	}
 
 }
