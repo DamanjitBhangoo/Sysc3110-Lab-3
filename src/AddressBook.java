12fe5fffa5c8e9407910c317809711f41b222fc0
damanjitbhangoo <damanjitbhangoo@aa0508-50.labs.sce.carleton.ca>
2016-09-20 12:06:55 -0400

    I added some function calls to test git

---
+++ src/AddressBook.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class AddressBook {
+
+	ArrayList<BuddyInfo> list = new ArrayList<BuddyInfo>();
+	
+	public void removeBuddy(BuddyInfo x) {
+		
+	}
+	
+	public void addBuddy(BuddyInfo x) {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
