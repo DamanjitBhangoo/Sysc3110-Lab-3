559fca9c6344de3a62820615367d5be37c005284
Damanjit Bhangoo <daman.9@hotmail.com>
2016-09-27 12:41:14 -0400

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,4 @@
-import java.util.*;
+eimport java.util.*;
 
 public class AddressBook {
 
@@ -24,7 +24,7 @@ public class AddressBook {
 		// TODO Auto-generated method stub
 		BuddyInfo x = new BuddyInfo("Potato");
 		AddressBook AB = new AddressBook();
-		System.out.println("Hello world!");
+		System.out.println("Hey World!");
 		AB.addBuddy(x);
 		AB.removeBuddy(0);
 	}
